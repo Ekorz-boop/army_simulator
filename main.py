fe39dfab1065afabe387f4eba80e4f604e62e8e9
Simon <muckot@gmail.com>
2023-04-24 14:21:19 +0200

    add skills and officers

--- main.py
+++ main.py
@@ -2,13 +2,35 @@ import random
 
 
 class Soldier:
-    def __init__(self, name, health, rank):
+    def __init__(self, name, health, rank, training, morale):
         self.name = name
         self.health = health
         self.rank = rank
+        self.training = training
+        self.morale = morale
+        self.fatigue = 0
+        self.disease = 0
+
+    def effectiveness(self):
+        return self.training * self.morale * (1 - self.fatigue) * (1 - self.disease)
 
     def __str__(self):
-        return f"{self.name}, {self.rank}, Health: {self.health}"
+        return f"{self.name}, {self.rank}, Health: {self.health}, Effectiveness: {self.effectiveness()}"
+
+
+class Officer(Soldier):
+    def __init__(self, name, health, rank, training, morale, leadership, skills=None):
+        super().__init__(name, health, rank, training, morale)
+        self.leadership = leadership
+        self.subordinates = []
+        self.skills = skills if skills else {}
+
+    def add_subordinate(self, soldier):
+        self.subordinates.append(soldier)
+
+    def __str__(self):
+        subordinates_str = ", ".join(soldier.name for soldier in self.subordinates)
+        return f"{super().__str__()}, Leadership: {self.leadership}, Skills: {self.skills}, Subordinates: [{subordinates_str}]"
 
 
 class Army:
@@ -45,21 +67,44 @@ def generate_random_name():
     return f"{random.choice(first_names)} {random.choice(last_names)}"
 
 
-def generate_random_soldier():
+def generate_random_soldier(is_officer=False):
     name = generate_random_name()
     health = random.randint(50, 100)
     rank_probabilities = [0.8, 0.15, 0.03, 0.015, 0.005]
     rank = random.choices(["Private", "Sergeant", "Lieutenant", "Captain", "Major"], rank_probabilities)[0]
-    return Soldier(name, health, rank)
+    training = random.uniform(0.5, 1)
+    morale = random.uniform(0.5, 1)
+
+    if is_officer:
+        leadership = random.uniform(0.5, 1)
+        skills = {"tactics": random.uniform(0, 1), "strategy": random.uniform(0, 1), "logistics": random.uniform(0, 1)}
+        return Officer(name, health, rank, training, morale, leadership, skills)
+    else:
+        return Soldier(name, health, rank, training, morale)
 
 
 def main():
     my_army = Army()
 
-    for _ in range(100):
-        my_army.add_soldier(generate_random_soldier())
+    # Create custom higher-ranking officers
+    custom_officers = [
+        Officer("George Washington", 100, "General", 0.9, 0.95, 0.9, {"tactics": 0.85, "strategy": 0.9, "logistics": 0.8}),
+        Officer("Horatio Gates", 100, "Major General", 0.8, 0.9, 0.85, {"tactics": 0.75, "strategy": 0.8, "logistics": 0.7})
+    ]
 
-    
+    for officer in custom_officers:
+        my_army.add_soldier(officer)
+
+    for _ in range(100):
+        soldier = generate_random_soldier()
+        my_army.add_soldier(soldier)
+        # Assign soldiers to officers based on their rank
+        if soldier.rank == "Private":
+            random.choice(custom_officers).add_subordinate(soldier)
+        elif soldier.rank == "Sergeant":
+            custom_officers[1].add_subordinate(soldier)
+        elif soldier.rank == "Lieutenant":
+            custom_officers[0].add_subordinate(soldier)
 
 
 if __name__ == "__main__":
