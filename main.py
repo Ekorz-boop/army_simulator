20307afb983895522e60282a44b178f8e1fcedb9
Simon <muckot@gmail.com>
2023-04-24 14:17:44 +0200

    more realistic percentages

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 class Soldier:
     def __init__(self, name, health, rank):
         self.name = name
@@ -8,6 +9,7 @@ class Soldier:
 
     def __str__(self):
         return f"{self.name}, {self.rank}, Health: {self.health}"
+
 
 class Army:
     def __init__(self):
@@ -18,22 +20,38 @@ class Army:
 
     def simulate_disease(self):
         for soldier in self.soldiers:
-            if random.random() < 0.1:
+            if random.random() < 0.3:  # Increased probability
                 soldier.health -= random.randint(1, 10)
+
+    def simulate_travel(self, distance):
+        for soldier in self.soldiers:
+            if random.random() < 0.1:  # Increased probability for fatigue
+                soldier.health -= random.randint(1, 5)
+            if random.random() < 0.05:  # Increased probability for injury
+                soldier.health -= random.randint(5, 10)
+            if random.random() < 0.03:  # Desertion instead of missing soldiers
+                self.soldiers.remove(soldier)
+
+        self.soldiers = [soldier for soldier in self.soldiers if soldier.health > 0]
+        self.simulate_disease()
 
     def __str__(self):
         return "\n".join(str(soldier) for soldier in self.soldiers)
+
 
 def generate_random_name():
     first_names = ["John", "Michael", "David", "James", "Robert"]
     last_names = ["Smith", "Johnson", "Williams", "Brown", "Jones"]
     return f"{random.choice(first_names)} {random.choice(last_names)}"
 
+
 def generate_random_soldier():
     name = generate_random_name()
     health = random.randint(50, 100)
-    rank = random.choice(["Private", "Sergeant", "Lieutenant", "Captain", "Major"])
+    rank_probabilities = [0.8, 0.15, 0.03, 0.015, 0.005]
+    rank = random.choices(["Private", "Sergeant", "Lieutenant", "Captain", "Major"], rank_probabilities)[0]
     return Soldier(name, health, rank)
+
 
 def main():
     my_army = Army()
@@ -41,13 +59,8 @@ def main():
     for _ in range(100):
         my_army.add_soldier(generate_random_soldier())
 
-    print("Army before disease:")
-    print(my_army)
+    
 
-    my_army.simulate_disease()
-
-    print("\nArmy after disease:")
-    print(my_army)
 
 if __name__ == "__main__":
     main()
