a5c65fff7b3dfc1c70a1c2b1dab7c4d51933cc81
Simon <muckot@gmail.com>
2023-04-24 14:40:11 +0200

    expand armies with subdivisions and more

--- main.py
+++ main.py
@@ -18,12 +18,57 @@ class Soldier:
         return f"{self.name}, {self.rank}, Health: {self.health}, Effectiveness: {self.effectiveness()}"
 
 
+class Infantry(Soldier):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.unit_type = "infantry"
+
+
+class Cavalry(Soldier):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.unit_type = "cavalry"
+
+
+class Artillery(Soldier):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.unit_type = "artillery"
+
+
+class Company:
+    def __init__(self, name, leader):
+        self.name = name
+        self.leader = leader
+        self.soldiers = []
+
+    def add_soldier(self, soldier):
+        self.soldiers.append(soldier)
+
+
+class Regiment:
+    def __init__(self, name, leader):
+        self.name = name
+        self.leader = leader
+        self.companies = []
+
+    def add_company(self, company):
+        self.companies.append(company)
+
+
 class Officer(Soldier):
     def __init__(self, name, health, rank, training, morale, leadership, skills=None):
         super().__init__(name, health, rank, training, morale)
         self.leadership = leadership
         self.subordinates = []
         self.skills = skills if skills else {}
+
+        if rank in ["Major", "General", "Major General"]:
+            self.skills.update({
+                "diplomacy": random.uniform(0, 1),
+                "intelligence": random.uniform(0, 1),
+                "recruitment": random.uniform(0, 1),
+            })
 
     def add_subordinate(self, soldier):
         self.subordinates.append(soldier)
@@ -45,13 +90,37 @@ class Army:
             if random.random() < 0.3:  # Increased probability
                 soldier.health -= random.randint(1, 10)
 
-    def simulate_travel(self, distance):
+    def simulate_travel(self, distance, terrain, weather, season):
+        terrain_factors = {
+            "flat": 1,
+            "hilly": 1.2,
+            "mountainous": 1.5,
+        }
+
+        weather_factors = {
+            "sunny": 1,
+            "rainy": 1.3,
+            "snowy": 1.7,
+        }
+
+        season_factors = {
+            "spring": 1,
+            "summer": 1,
+            "fall": 1.2,
+            "winter": 1.5,
+        }
+
+        terrain_factor = terrain_factors.get(terrain, 1)
+        weather_factor = weather_factors.get(weather, 1)
+        season_factor = season_factors.get(season, 1)
+        total_factor = terrain_factor * weather_factor * season_factor
+
         for soldier in self.soldiers:
-            if random.random() < 0.1:  # Increased probability for fatigue
+            if random.random() < 0.1 * total_factor:  # Increased probability for fatigue
                 soldier.health -= random.randint(1, 5)
-            if random.random() < 0.05:  # Increased probability for injury
+            if random.random() < 0.05 * total_factor:  # Increased probability for injury
                 soldier.health -= random.randint(5, 10)
-            if random.random() < 0.03:  # Desertion instead of missing soldiers
+            if random.random() < 0.03 * total_factor:  # Desertion instead of missing soldiers
                 self.soldiers.remove(soldier)
 
         self.soldiers = [soldier for soldier in self.soldiers if soldier.health > 0]
@@ -70,17 +139,62 @@ def generate_random_name():
 def generate_random_soldier(is_officer=False):
     name = generate_random_name()
     health = random.randint(50, 100)
-    rank_probabilities = [0.8, 0.15, 0.03, 0.015, 0.005]
-    rank = random.choices(["Private", "Sergeant", "Lieutenant", "Captain", "Major"], rank_probabilities)[0]
+    rank = "Private"
     training = random.uniform(0.5, 1)
     morale = random.uniform(0.5, 1)
+
+    unit_probabilities = [0.6, 0.3, 0.1]
+    unit_type = random.choices(["infantry", "cavalry", "artillery"], unit_probabilities)[0]
 
     if is_officer:
         leadership = random.uniform(0.5, 1)
         skills = {"tactics": random.uniform(0, 1), "strategy": random.uniform(0, 1), "logistics": random.uniform(0, 1)}
-        return Officer(name, health, rank, training, morale, leadership, skills)
+        officer = Officer(name, health, rank, training, morale, leadership, skills)
+        officer.unit_type = unit_type
+        return officer
     else:
-        return Soldier(name, health, rank, training, morale)
+        if unit_type == "infantry":
+            return Infantry(name, health, rank, training, morale)
+        elif unit_type == "cavalry":
+            return Cavalry(name, health, rank, training, morale)
+        elif unit_type == "artillery":
+            return Artillery(name, health, rank, training, morale)
+
+
+def generate_army(size):
+    army = Army()
+
+    regiment = Regiment("1st Infantry Regiment", None)
+    company = Company("A Company", None)
+
+    for _ in range(size):
+        rank_probabilities = [0.8, 0.15, 0.03, 0.015, 0.005, 0.001, 0.001]
+        rank = random.choices(
+            ["Private", "Sergeant", "Lieutenant", "Captain", "Major", "General", "Major General"],
+            rank_probabilities,
+        )[0]
+
+        soldier = generate_random_soldier(rank in ["Major", "General", "Major General"])
+
+        if rank in ["Major", "General", "Major General"]:
+            officer = soldier
+            officer.rank = rank
+
+            if rank == "Major General":
+                regiment.leader = officer
+            elif rank == "Major":
+                regiment = Regiment(f"{officer.name}'s Regiment", officer)
+                army.add_soldier(officer)
+            elif rank == "Captain":
+                company = Company(f"{officer.name}'s Company", officer)
+                regiment.add_company(company)
+                army.add_soldier(officer)
+        else:
+            soldier.rank = rank
+            company.add_soldier(soldier)
+            army.add_soldier(soldier)
+
+    return army
 
 
 def main():
